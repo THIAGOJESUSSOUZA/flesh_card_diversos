73af953d3ebccdaf1c16d04904c33bcd7a553040
THIAGOJESUSSOUZA <161726010+THIAGOJESUSSOUZA@users.noreply.github.com>
2025-02-13 10:42:55 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,71 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é a ferramenta que permite criar um protótipo de alta fidelidade?',
+    'FIGMA'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é um protótipo?',
+    'Plano ou projeto de trabalho'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Programação',
+    'O que é um protótipo de baixa fidelidade?',
+    'É feito em papel e caneta geralmente, e ajuda a ganhar tem ao expor as primeiras ideias'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um protótipo de Alta fidelidade?',
+    'É desenvolvido por ferramentas próprias e muito parecido com o real'
+)
+
+criaCartao(
+    'Programação',
+    'Para usar o figma tem que se cadastrar. Verdade ou mentira',
+    'Verdade'
+)
+
+criaCartao(
+    'inglês',
+    'O que é significa o botão "new design file" ?',
+    'novo arquivo de projeto'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o tamanho da tela (frame) foi sugerido?',
+    'Android large'
+)
+
+criaCartao(
+    'Programação',
+    'Qual tecla de atalho para o retângulo?',
+    'A tecla R'
+)
+
+criaCartao(
+    'Programação',
+    'quando clicamos na ferramenta elipse que representa o circulo qual é o atalho que podemos usar?',
+    'Podemos usar a tecla O'
+)
+
+criaCartao(
+    'iNGLÊS',
+    'O que significa Fill, e o que faz?',
+    'Preenchimento, muda a cor de fundo'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é o nome do Professor?',
+    'Thiago'
+)
+
+criaCartao(
+    'Geral',
+    'qual o nome da diciplina que estamos estudando" ?',
+    'Matemática II ou Programação'
 )
